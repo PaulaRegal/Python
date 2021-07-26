fedbbc289ec657370ae38cfff7c215edc1ec6b66
Paula Regal <paulacregal@yahoo.com.br>
2021-07-26 08:00:19 -0300

    Calculadora e Televisao

---
+++ Metodos funcoes classes/Televisão.py
@@ -0,0 +1,34 @@
+class Televisao:
+    def __init__(self):
+        self.ligada = False #tv vai começar desligada
+        self.canal = 5 #canal vai começar no 5
+
+#metodo para ligar a tv
+    def power(self):
+        if self.ligada:
+            self.ligada = False
+        else:
+            self.ligada = True
+
+    def aumenta_canal(self):
+        if self.ligada:
+            self.canal += 1
+
+    def diminui_canal(self):
+        if self.ligada:
+            self.canal -= 1
+
+televisao = Televisao()
+print('Televisão está ligada: {}'.format(televisao.ligada))
+televisao.power()
+print('Televisão está ligada: {}'.format(televisao.ligada))
+televisao.power()
+print('Televisão está ligada: {}'.format(televisao.ligada))
+print('Canal: {}'.format(televisao.canal))
+televisao.power() # para ligar a tv
+print('Televisão está ligada: {}'.format(televisao.ligada))
+televisao.aumenta_canal()
+televisao.aumenta_canal()
+print('Canal: {}'.format(televisao.canal))
+televisao.diminui_canal()
+print('Canal: {}'.format(televisao.canal))
