d4c39cd654a2667bf67eac1c9455305fb658fabe
Paula Regal <paulacregal@yahoo.com.br>
2021-09-06 17:13:17 -0300

    ocultador_de_arquivos

---
+++ Segurança da Informação/ocultador_de_arquivos.py
@@ -0,0 +1,23 @@
+import ctypes
+
+atributo_ocultar = 0x02
+
+retorno = ctypes.windll.kernel32.SetFileAttributesW('ocultar.txt', atributo_ocultar)
+
+if retorno:
+    print("Arquivo foi ocultado")
+else:
+    print("Arquivo não foi ocultado")
+
+
+#se quiser ocultar uma pasta:
+pasta = input('Digite o caminho da pasta a ser ocultada ex: C:/pasta')
+
+atributo_ocultar = 0x02
+
+retorno = ctypes.windll.kernel32.SetFileAttributesW('pasta', atributo_ocultar)
+
+if retorno:
+    print("A pasta foi ocultada")
+else:
+    print("A pasta não foi ocultada")
